492c8e489dc4b9f18ea02b9782779087449c69db
asharij13 <156457087+asharij13@users.noreply.github.com>
2025-02-20 17:06:23 -0500

    changed table

--- database/user.sql
+++ database/user.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users(
-    user_id INTEGER PRIMARY KEY,
-    user_name VARCHAR,
-    user_email VARCHAR,
-    user_password VARCHAR,
-    user_contact_details TEXT,
-    user_preferences TEXT,
-    user_status VARCHAR,
+    user_id INTEGER(7) PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(20) NOT NULL,
+    user_email VARCHAR(50) NOT NULL,
+    user_password VARCHAR(60) NOT NULL,
+    user_contact_details VARCHAR(50) NOT NULL,
+    user_preferences TEXT NOT NULL,
+    user_status ENUM('Active', 'Warning', 'Suspended') NOT NULL,
     user_created TIMESTAMP,
     user_last_updated TIMESTAMP
 );
