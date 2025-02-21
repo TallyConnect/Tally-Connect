361ef1ea276fa31b38eecd68015f207a250e1174
Tosin-Jolaogun <tosinjolaogun@gmail.com>
2025-02-20 23:37:56 -0500

    Update user_role.sql

--- database/user_role.sql
+++ database/user_role.sql
@@ -1,11 +1,10 @@
 CREATE TABLE user_roles (
     user_id INTEGER(7),
-    role_id ENUM('Moderator', 'Organizer', 'Adminastrator', 'User')R,
-    PRIMARY KEY (user_id, role_id),
+    role_id ENUM('Moderator', 'Organizer', 'Adminastrator', 'User'),
     FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
 
 INSERT INTO user_roles (user_id, role_id) VALUES
-(1, 1),
-(2, 2);
+(1, 'Admin'),
+(2, 'Organizer'),
+(3, 'Moderator');
