cde9410d945474af1dfb885a97a3369953c9ecf0
Tosin-Jolaogun <tosinjolaogun@gmail.com>
2025-04-03 19:55:22 -0400

    events for table creation and database configuration

---
+++ database/eventlist.sql
@@ -0,0 +1,71 @@
+CALL insert_event(
+   'JaneSmith',
+   'Community BBQ',
+   'A fun BBQ event for the neighborhood',
+   'Central Park',
+   '2025-03-01',
+   '12:00:00',
+   'Scheduled',
+   '2ac927c9_Summer-Party-Flyer-preview.jpg',
+   'Approved'
+);
+
+CALL insert_event(
+   'SophiaMartinez',
+   'Study Session at Coleman Library',
+   'A focused study session at Coleman Library for students!',
+   'Coleman Library, FAMU',
+   '2024-12-03',
+   '10:00:00',
+   'Scheduled',
+   '2ac927c9_Summer-Party-Flyer-preview.jpg',
+   'Approved'
+);
+
+CALL insert_event(
+   'Second Harvest',
+   'Find Food Near You Pick-up Volunteers',
+   'Volunteers needed to distribute food.',
+   '4446 Entrepot Blvd',
+   '2025-01-04',
+   '13:00:00',
+   'Scheduled',
+   '2ac927c9_Summer-Party-Flyer-preview.jpg',
+   'Approved'
+);
+
+CALL insert_event(
+   'DanielClark',
+   'Volunteer at Cascades Park',
+   'Join us to clean up Cascades Park and make a positive impact.',
+   'Cascades Park, Tallahassee',
+   '2025-03-03',
+   '09:00:00',
+   'Scheduled',
+   '2ac927c9_Summer-Party-Flyer-preview.jpg',
+   'Approved'
+);
+
+CALL insert_event(
+   'FAMU-FSU NSBE',
+   'Tech & Leadership Conference',
+   'An interactive conference on technology and leadership.',
+   'FSU Innovation Hub',
+   '2025-05-10',
+   '14:00:00',
+   'Scheduled',
+   '2ac927c9_Summer-Party-Flyer-preview.jpg',
+   'Approved'
+);
+
+CALL insert_event(
+   'DavidWilson',
+   'Test 1',
+   'Testing',
+   'Test Center',
+   '2025-03-21',
+   '19:15:00',
+   'Draft',
+   '/static/flyers/92679e58_testing_banner.jpg',
+   'Pending'
+);
--- database/events.sql
+++ database/events.sql
@@ -6,9 +6,10 @@ CREATE TABLE events (
     event_location VARCHAR(50) NOT NULL,
     event_date DATE NOT NULL,
     event_time TIME NOT NULL,
-    event_status ENUM('Scheduled', 'Canceled', 'Completed') NOT NULL,
+    event_status ENUM('Scheduled', 'Canceled', 'Completed', 'Draft') NOT NULL DEFAULT 'Draft',
     event_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     event_last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    moderator_approval ENUM('Pending', 'Approved', 'Denied') DEFAULT 'Pending',
     flyer_url VARCHAR(500),
     PRIMARY KEY (event_id), 
     FOREIGN KEY (user_name) REFERENCES users(user_name) ON DELETE CASCADE
