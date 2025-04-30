5277a1bda21c5cd0fffe2ee07b3cc360767f9a25
Tosin-Jolaogun <tosinjolaogun@gmail.com>
2025-04-29 21:27:41 -0400

    added a table in event tags so that old events are tagged like the new events
    resource eventtags.sql

--- database/eventtags.sql
+++ database/eventtags.sql
@@ -5,3 +5,9 @@ CREATE TABLE event_tags (
     FOREIGN KEY (event_id) REFERENCES events(event_id),
     FOREIGN KEY (tag_id) REFERENCES event_categories(category_id)
 );
+
+INSERT INTO event_tags (event_id, tag_id)
+SELECT e.event_id, c.category_id
+FROM events e
+JOIN event_categories c ON e.category_name = c.category_name
+WHERE e.category_name IS NOT NULL;
